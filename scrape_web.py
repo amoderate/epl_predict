6d74b975aaa5b4b61aaa18458f598c52abd710e3
noise <noise.roc@gmail.com>
2014-03-01 21:06:15 -0500

    fix scrape

--- scrape_web.py
+++ scrape_web.py
@@ -2,11 +2,18 @@ import urllib2
 import csv
 import numpy as np
 
-
+def replace_value(value):
+	#print value
+	aux = 0
+	if value == 'H':
+		aux = 1
+	elif value == 'A':
+		aux = 2
+	return str(aux)
 #Open dataset
 
 
-def extract_epl_features(data):
+def extract_epl_features(data, year):
 #extract useful features
 	row_num = 0
 	header = []
@@ -14,34 +21,75 @@ def extract_epl_features(data):
 	target_header = []
 	data_list = []
 
-	for row in data:
-		row_list = []
-		header_list = []
-		if row_num == 0:
-			#header.append(row)
-			header_list.extend(row[1:4])
-			header_list.extend(row[4:6])
-			header_list.extend(row[7:9])
-			header_list.extend(row[11:18])
-			header.append(header_list)
-			target_header.append(row[6])
+	if year == '1213':
+
+		for row in data:
+			row_list = []
+			header_list = []
+			target_header_list = []
+			target_b_list = []
+
+			if row_num == 0:
+				#header.append(row)
+				header_list.extend(row[1:4])
+				header_list.extend(row[4:6])
+				header_list.extend(row[7:9])
+				header_list.extend(row[10:18])
+				header.append(header_list)
+				target_header_list.extend(row[1:3])
+				target_header_list.extend(row[6:7])
+				target_header.append(target_header_list)
+					
+			else:
+				row_list.extend(row[1:4])
+				row_list.extend(row[4:6])
+				row_list.extend(row[7:9])
+				row_list.extend(row[10:18])
+				data_list.append(row_list)
 				
-		else:
-			row_list.extend(row[1:4])
-			row_list.extend(row[4:6])
-			row_list.extend(row[7:9])
-			row_list.extend(row[11:18])
-			data_list.append(row_list)
-			target_list.append(row[6])
+				target_b_list.extend(row[1:3])
+				target_b_list.extend(replace_value(row[6:7][0]))
+				target_list.append(target_b_list)
 
-		row_num += 1
+			row_num += 1
+	else:
+		for row in data:
+			row_list = []
+			header_list = []
+			target_header_list = []
+			target_b_list = []
+			if row_num == 0:
+				#header.append(row)
+				header_list.extend(row[1:4])
+				header_list.extend(row[4:6])
+				header_list.extend(row[7:9])
+				header_list.extend(row[11:19])
+				header.append(header_list)
 
+				target_header_list.extend(row[1:3])
+				target_header_list.extend(row[6:7])
+				target_header.append(target_header_list)
+					
+			else:
+				row_list.extend(row[1:4])
+				row_list.extend(row[4:6])
+				row_list.extend(row[7:9])
+				row_list.extend(row[11:19])
+				data_list.append(row_list)
+				
+				target_b_list.extend(row[1:3])
+				target_b_list.extend(replace_value(row[6:7][0]))
+				target_list.append(target_b_list)
+
+			row_num += 1
+
+	#print [(number, item) for (number, item) in enumerate(header[0])]
 	return header, data_list, target_header, target_list
 
 
 #save output as csv for further processing
-def write_to_file(data_out, data_in):
-	header, data, target_header, target_list = extract_epl_features(data_in)
+def write_to_file(data_out, data_in,year):
+	header, data, target_header, target_list = extract_epl_features(data_in, year)
 	#write features to file
 	c = csv.writer(open(data_out + '.csv', "wb"))
 
@@ -51,23 +99,24 @@ def write_to_file(data_out, data_in):
 	#write target to file
 
 	c = csv.writer(open(data_out +'_target.csv', "wb"))
-	c.writerow(target_header)
+	c.writerow(target_header[0])
 
 	for row in target_list:
 		c.writerow(row)
 
 
 def main():
-	list_of_years = ['1314', '1213', '1112', '1011','0910','0809']
+	list_of_years = ['1314', '1213', '1112', '1011','0910','0809', '0708', '0607','0506', '0405', '0304']
+
 	for year in list_of_years:
 		response = urllib2.urlopen('http://www.football-data.co.uk/mmz4281/'+year+'/E0.csv')
 #import csv
 		data_in = csv.reader(response)
 
 #export data
-		data_out = 'processed_epl_data_' + year
+		data_out = 'processed_data/processed_epl_data_' + year
 
-		write_to_file(data_out, data_in)
+		write_to_file(data_out, data_in, year)
 
 if __name__ =="__main__":
 	main()
@@ -76,7 +125,6 @@ if __name__ =="__main__":
 #########################################
 #Utility functions below here!
 
-#print [(number, item) for (number, item) in enumerate(header[0])]
 #print header[0]
 
 # data_list = []
